1de566189a87ec8b9ba565aa5a92dcc5a999cf8f
Italo Carlo <italocarlo@nti.ufal.br>
2021-10-13 16:36:24 -0300

    Database config creation

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -9,11 +9,23 @@ interface IOptions {
 //  newOptions.host = 'database_ignite'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados  
 //});
 
-export default async(host = "database_ignite"): Promise<Connection> => {  
+/*export default async(host = "database_ignite"): Promise<Connection> => {  
   const defaultOptions = await getConnectionOptions();    
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === 'test' ? "localhost" : host,
+      database: process.env.NODE_ENV === 'test' 
+      ? "rentx_test" : defaultOptions.database
+
+    })
+  );
+};*/
+
+
+export default async(): Promise<Connection> => {  
+  const defaultOptions = await getConnectionOptions();    
+  return createConnection(
+    Object.assign(defaultOptions, {      
       database: process.env.NODE_ENV === 'test' 
       ? "rentx_test" : defaultOptions.database
 
